83e158736f09ffd1061bcad2e0fe574e0349ca6c
yahyasaadi <yahyasaadi9219@gmail.com>
2021-11-13 07:21:06 +0300

    Added an event listener

--- js/main.js
+++ js/main.js
@@ -2,4 +2,9 @@ const day = document.getElementById('day').value;
 const month = document.getElementById('month').value;
 const year = document.getElementById('year').value;
 const gender = document.getElementsByName('gender').value;
-const form = document.getElementById('form').value;
+const form = document.getElementById('form');
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault()
+})
+
