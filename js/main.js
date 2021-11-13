4e9b00fb565b8fe9e64fd9e1ea318e6ecf14b7e1
yahyasaadi <yahyasaadi9219@gmail.com>
2021-11-13 07:12:02 +0300

    Added initial js scripts

---
+++ js/main.js
@@ -0,0 +1,5 @@
+const day = document.getElementById('day').value;
+const month = document.getElementById('month').value;
+const year = document.getElementById('year').value;
+const gender = document.getElementsByName('gender').value;
+const form = document.getElementById('form').value;
