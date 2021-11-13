51645a7e583cede2ed63b7d4f0958c06fd067e1b
yahyasaadi <yahyasaadi9219@gmail.com>
2021-11-13 14:29:55 +0300

    Add a function to generate birth day to main.js

--- js/main.js
+++ js/main.js
@@ -4,7 +4,21 @@ const year = document.getElementById('year').value;
 const gender = document.getElementsByName('gender').value;
 const form = document.getElementById('form');
 
+
+function generateDay(day, month, year) {
+	const offSetTable = [0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4];
+  if (month < 3){
+  	year = year - 1;
+  }
+  result = (year + year/4 - year/100 + year/400 + offSetTable[month-1] + day) % 7;
+  return Math.floor(result);
+}
+
+
+
 form.addEventListener('submit', function (e) {
     e.preventDefault()
 })
+
+
 
